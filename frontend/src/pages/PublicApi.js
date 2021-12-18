1f60096b5a33ba5a6c868030c1fa14cc6dc09643
Noah Janzen <noah.janzen@hs-ruhrwest.de>
2021-12-18 13:24:14 +0100

    Extends public API description

--- frontend/src/pages/PublicApi.js
+++ frontend/src/pages/PublicApi.js
@@ -7,6 +7,11 @@ function PublicApi(props) {
             <p className="pb-3">
                 In der Tabelle sind historische Kurse der Kryptowährung Bitcoin
                 in Euro aufgeführt.
+                <br />
+                Die Daten stammen von der API von CryptoCompare.{' '}
+                <a href="https://min-api.cryptocompare.com/documentation?key=Historical&cat=dataHistoday">
+                    Zur API-Dokumentation
+                </a>
             </p>
             <BitcoinHistory />
         </div>
