248812aae8ff8e0c273eaa8e79587bb7c11a3025
Noah Janzen <noah.janzen@hs-ruhrwest.de>
2021-12-18 13:02:39 +0100

    Fixes typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './layout/Layout'
 import ExpressApi from './pages/ExpressApi'
-import FirebaseApi from './pages/FirevaseApi'
+import FirebaseApi from './pages/FirebaseApi'
 import Index from './pages/Index'
 import PublicApi from './pages/PublicApi'
 
