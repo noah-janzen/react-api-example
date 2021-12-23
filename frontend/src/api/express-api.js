200003fafbd2a2cb85099cfd047e7c05d43195ad
Noah Janzen <noah.janzen@hs-ruhrwest.de>
2021-12-23 11:38:45 +0100

    FE: Adds API functions

--- frontend/src/api/express-api.js
+++ frontend/src/api/express-api.js
@@ -1,5 +1,37 @@
-const API_BACKEND_URL = 'http://localhost:8080/api/'
+const API_BACKEND_URL = 'http://localhost:8080/api'
 
 export function getBooks() {
-    return fetch(`${API_BACKEND_URL}books`).then((response) => response.json())
+    return fetch(`${API_BACKEND_URL}/books`).then((response) => response.json())
+}
+
+export function getBook(id) {
+    return fetch(`${API_BACKEND_URL}/books/${id}`).then((response) =>
+        response.json()
+    )
+}
+
+export function addBook(book) {
+    return fetch(`${API_BACKEND_URL}/books`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(book),
+    }).then((response) => response.json())
+}
+
+export function editBook(id, book) {
+    return fetch(`${API_BACKEND_URL}/books/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(book),
+    }).then((response) => response.json())
+}
+
+export function deleteBook(bookId) {
+    return fetch(`${API_BACKEND_URL}/books/${bookId}`, {
+        method: 'DELETE',
+    })
 }
